b2cb1ea67e13d27a8bdc83fa7234c5c169d8dd5b
d.cioffo <d.cioffo@studenti.unipi.it>
2020-12-10 16:10:52 +0100

    Modifica del controller per gli snapshot

--- JustRecipe/src/main/java/it/unipi/dii/inginf/lsdb/justrecipe/controller/RecipeSnapshotController.java
+++ JustRecipe/src/main/java/it/unipi/dii/inginf/lsdb/justrecipe/controller/RecipeSnapshotController.java
@@ -7,9 +7,11 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
 import javafx.scene.text.Text;
 
+import java.awt.event.ActionEvent;
 import java.io.IOException;
 import java.util.List;
 
@@ -17,41 +19,36 @@ public class RecipeSnapshotController {
     private final int HOW_MANY_CHAR_SNAPSHOT_TITLE = 25;
     private final int HOW_MANY_CHAR_SNAPSHOT_CATEGORIES = 50;
 
+    @FXML private Text snapTitle;
+    @FXML private Text snapUser;
+    @FXML private Text snapCarbs;
+    @FXML private Text snapCal;
+    @FXML private Text snapFat;
+    @FXML private Text snapProtein;
+    @FXML private ImageView snapImg;
+    @FXML private Text snapCategories;
 
-    /**
-     * It create a snapshot for the recipe with these values
-     * @param recipe    The recipe to show
-     * @return The pane that is the snapshot of the recipe
-     */
-    public Pane createSnapRecipe (Recipe recipe)
+    private Recipe recipe; // recipe shows in this snapshot
+
+    public void initialize ()
     {
-        Pane newSnap = getRecipesSnapshotFXML();
 
-        // Title
-        Text t = (Text) newSnap.getChildren().get(3);
+    }
+
+    public void setRecipe (Recipe recipe)
+    {
+        this.recipe = recipe;
         String title = recipe.getTitle();
         if (title.length() > HOW_MANY_CHAR_SNAPSHOT_TITLE)
         {
             title = title.substring(0, HOW_MANY_CHAR_SNAPSHOT_TITLE-1) + "...";
         }
-        t.setText(title);
-
-        // User
-        Text u = (Text) newSnap.getChildren().get(1);
-        u.setText(recipe.getAuthorUsername());
-        // Carbs
-        Text c = (Text) newSnap.getChildren().get(9);
-        c.setText(String.valueOf(recipe.getCarbs()));
-        // Protein
-        Text p = (Text) newSnap.getChildren().get(13);
-        p.setText(String.valueOf(recipe.getProtein()));
-        // Fat
-        Text f = (Text) newSnap.getChildren().get(10);
-        f.setText(String.valueOf(recipe.getFat()));
-        // Calories
-        Text cal = (Text) newSnap.getChildren().get(8);
-        p.setText(String.valueOf(recipe.getCalories()));
-        // Categories
+        snapTitle.setText(title);
+        snapUser.setText(recipe.getAuthorUsername());
+        snapCarbs.setText(String.valueOf(recipe.getCarbs()));
+        snapProtein.setText(String.valueOf(recipe.getProtein()));
+        snapFat.setText(String.valueOf(recipe.getFat()));
+        snapCal.setText(String.valueOf(recipe.getCalories()));
         String overallTags = "";
         for (String s: recipe.getCategories()) {
             overallTags = overallTags.concat(s).concat(", ");
@@ -60,31 +57,14 @@ public class RecipeSnapshotController {
         {
             overallTags = overallTags.substring(0, HOW_MANY_CHAR_SNAPSHOT_CATEGORIES-1) + "...";
         }
-        Text categ = (Text) newSnap.getChildren().get(11);
-        categ.setText(overallTags);
-        // Recipe's pic
-        ImageView recipefoto = (ImageView) newSnap.getChildren().get(2);
-        recipefoto.setImage(new Image("img/genericRecipe.png"));
-
-        return newSnap;
-    }
-
-    /**
-     * It retrieves the fxml file for the recipe snap
-     * @return The default pane for a default recipe snap
-     */
-    public Pane getRecipesSnapshotFXML()
-    {
-        Pane view = null;
-        try
+        snapCategories.setText(overallTags);
+        if (recipe.getPicture() != null)
         {
-            FXMLLoader loader = new FXMLLoader(Utils.class.getResource("/recipeSnap.fxml"));
-            view = (Pane)loader.load();
+            snapImg.setImage(new Image(recipe.getPicture()));
         }
-        catch (IOException e)
+        else
         {
-            e.printStackTrace();
+            snapImg.setImage(new Image("img/genericRecipe.png"));
         }
-        return view;
     }
 }
