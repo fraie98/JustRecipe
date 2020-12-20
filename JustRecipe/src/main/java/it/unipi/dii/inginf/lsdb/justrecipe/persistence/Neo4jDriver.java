48a4745e83116cb399c00fd8b0e0a6827ed5f292
fraie98 <francesco.iemma11@gmail.com>
2020-12-20 12:07:16 +0100

    Bug related to addRecipe in Neo4j fixed

--- JustRecipe/src/main/java/it/unipi/dii/inginf/lsdb/justrecipe/persistence/Neo4jDriver.java
+++ JustRecipe/src/main/java/it/unipi/dii/inginf/lsdb/justrecipe/persistence/Neo4jDriver.java
@@ -87,9 +87,9 @@ public class Neo4jDriver implements DatabaseDriver{
             session.writeTransaction((TransactionWork<Void>) tx -> {
                 tx.run( "UNWIND $props as rnew " +
                                 "MATCH (u:User{username:$name}) " +
-                                "MERGE (rec:Recipe{title:rnew.title,calories:rnew.calories,fat:rnew.fat," +
+                                "CREATE (rec:Recipe{title:rnew.title,calories:rnew.calories,fat:rnew.fat," +
                                 "protein:rnew.protein,carbs:rnew.carbs}) " +
-                                "MERGE (u)-[ad:ADDS]->(rec) " +
+                                "CREATE (u)-[ad:ADDS]->(rec) " +
                                 "SET ad.when=$ts",
                         parameters( "props", query, "name", r.getAuthorUsername(), "ts", r.getCreationTime().getTime()) );
                 return null;
